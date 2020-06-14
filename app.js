3754bf0978a9886be4272367e9c3fd4cc0dcf902
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2020-06-15 00:24:30 +0100

    fix bug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const links = document.querySelector('.links')
+const links = document.querySelector(".links");
 const navBtn = document.querySelector(".nav-toggle");
 const closeBtn = document.querySelector(".work-modal-close");
 const modal = document.querySelector(".work-modal");
@@ -6,46 +6,46 @@ const modalItem = document.querySelector(".work-modal-item");
 
 //navbar
 navBtn.addEventListener("click", function () {
-    links.classList.toggle("show-links");
+  links.classList.toggle("show-links");
 });
 
-
-
 const eventListeners = (() => {
-    class UI {
-        //show modal
-        showModal(event) {
-        event.preventDefault();
-        if (event.target.parentElement.classList.contains("work-item-icon")) {
-            let id = event.target.parentElement.dataset.id;
+  class UI {
+    //show modal
+    showModal(event) {
+      event.preventDefault();
+      if (event.target.parentElement.classList.contains("work-item-icon")) {
+        let id = event.target.parentElement.dataset.id;
 
-            modal.classList.add("show-modal");
-            modalItem.style.backgroundImage = `url(./img/galeria-${id}.jpg)`;
-            }
-        }
-
-        //hide modal
-        closeModal(event) {
-        event.preventDefault();
-        modal.classList.remove("show-modal");
-        }
+        modal.classList.add("show-modal");
+        modalItem.style.backgroundImage = `url(./img/galeria-${id}.jpg)`;
+      }
     }
 
-    const ui = new UI();
-
-    //display modal
-    const icons = document.querySelectorAll(".work-item-icon");
-    icons.forEach((item) => {
-        item.addEventListener("click", function (event) {
-        ui.showModal(event);
-        });
-    });
-
     //hide modal
-    closeBtn.addEventListener("click", function () {
-        ui.closeModal(event);
+    closeModal(event) {
+      event.preventDefault();
+      modal.classList.remove("show-modal");
+    }
+  }
+
+  const ui = new UI();
+
+  //display modal
+  const icons = document.querySelectorAll(".work-item-icon");
+  icons.forEach(item => {
+    item.addEventListener("click", function (event) {
+      ui.showModal(event);
     });
-    
+  });
+
+  // hide modal
+  closeBtn.addEventListener("click", function () {
+    ui.closeModal(event);
+  });
+  //   window.addEventListener("click", function (event) {
+  //     if (event.target.contains("show-modal")) {
+  //       ui.closeModal(event);
+  //     }
+  //   });
 })();
-
-
